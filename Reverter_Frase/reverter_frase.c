dd3c676c6a3ef1c1bb51706f814a3f0cb25782da
GuiMatheus1313 <guilhermematheusdejesusaraujo6@gmail.com>
2023-07-07 17:34:31 -0300

    Criação da função sgets
    Com o objetivo de remover o 'ENTER' gerado pelo fgets

---
+++ Reverter_Frase/reverter_frase.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <string.h>
+
+//Função feita para remover o 'ENTER' inserido pela função fgets
+int Sget(char *vetor, int TAM)
+{
+    fflush(stdin);
+    if(fgets(vetor, TAM, stdin))
+    {
+        int i;
+        for(i=0; vetor[i] != '\n' && vetor[i]; i++)
+            ;
+        vetor[i] = '\0';
+    }
+}
+
+int main()
+{
+
+}
