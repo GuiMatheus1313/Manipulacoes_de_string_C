9440755dcf0cfeaeb2dc5a9684a088ee9dfd2d65
GuiMatheus1313 <guilhermematheusdejesusaraujo6@gmail.com>
2023-07-09 19:06:01 -0300

    Reformulação + comentários para o reverter_frase

--- Reverter_Frase/reverter_frase.c
+++ Reverter_Frase/reverter_frase.c
@@ -16,16 +16,26 @@ int Sget(char *vetor, int TAM)
 
 void ReverteFrase(char frase[], int tamanho)
 {
-    int i = 0, j = 0;
+    //Variáveis de controle para loops
+    int i = 0, j = 0, k = 0;
 
+    //Loop para percorrer de trás para frente
     for(i = tamanho-1; i >= 0; i--)
     {
+        //Condição para formular a inversão após cada espaço
         if(frase[i] == ' ')
         {
+            //Após o espaço, adaptará as letras para reverte-las 
             for(j = i+1; j < tamanho && frase[j] != ' '; j++)
                 printf("%c", frase[j]);
+            printf(" ");
         }
+        
     }
+
+    //Loop especificado para a última palavra/letra
+    for(;frase[k] != ' ' && k < tamanho; k++)
+      printf("%c", frase[k]);
 }
 
 #define tamanho 200
@@ -33,11 +43,13 @@ int main()
 {
     int tam_atual = 0;
     char frase_original[tamanho];
-    char frase_revertida[tamanho];
     
-    printf("Digita uma frase de até 199 caracteres para reverte-la\n");
+    printf("Digita uma string de até 199 caracteres para reverte-la\n");
+
+    //Função que adapta o fgets()
     Sget(frase_original, tamanho);
     
+    //Adquire o tamanho atual da string e inicia o procedimento de reverter a frase
     tam_atual = strlen(frase_original);
-    ReverteFrase(frase_original, tamanho);
+    ReverteFrase(frase_original, tam_atual);
 }
